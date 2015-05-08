4c90ebbd65e038a71389671edacebd4af3256383
Teemu Kokkonen <teemu.on.ihminen@gmail.com>
2015-05-08 12:34:40 +0300

    Remove lots of old code

--- manage.py
+++ manage.py
@@ -2,33 +2,12 @@
 from flask.ext.script import Manager
 from flask.ext.collect import Collect
 
-from app import app, db, models
+from app import app
 
 manager = Manager(app)
 collect = Collect()
 collect.init_app(app)
 collect.init_script(manager)
-
-
-@manager.command
-def initialize_database():
-    "Drop current database and initialize a new one"
-    db.drop_all()
-    db.create_all()
-    admin = models.Admin(app.config['USERNAME'], app.config['PASSWORD'])
-    db.session.add(admin)
-    db.session.commit()
-
-    print "Database initialized"
-
-
-@manager.command
-def add_test_user():
-    user = models.SignUp("Test User", "test.user@gmail.com", "Aalto-yliopisto TiK", "Vahan.", False, False, True)
-    db.session.add(user)
-    db.session.commit()
-
-    print "User added"
 
 
 if __name__ == "__main__":
