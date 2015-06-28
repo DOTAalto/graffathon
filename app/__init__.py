5901f055884f8b981a93fff56f9fb0b53b2e5d2b
Teemu Kokkonen <teemu.on.ihminen@gmail.com>
2015-06-28 13:48:55 +0300

    Fix 27. & 28.

--- app/__init__.py
+++ app/__init__.py
@@ -45,7 +45,40 @@ RESULTS = [
   [31,74,u'Sähköjänis - JuMiPallot'],
   [32,64,u'DX9 4k Framework - @RemesJann'],
 ]
-LINKS = ["https://www.youtube.com/watch?v=XEAE0FyNERA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=1", "https://www.youtube.com/watch?v=pWyp6SAllY4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=2", "https://www.youtube.com/watch?v=InN-bmRk2lQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=3", "https://www.youtube.com/watch?v=bYpXWAsFYvM&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=4", "https://www.youtube.com/watch?v=XucfklQa6iA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=5", "https://www.youtube.com/watch?v=UC-lTdqXrh4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=6", "https://www.youtube.com/watch?v=O7qWdIpcNTs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=7", "https://www.youtube.com/watch?v=1ctdbP-ySWI&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=8", "https://www.youtube.com/watch?v=4GQ2yo7KkEs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=9", "https://www.youtube.com/watch?v=7Jq9b5nVhe4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=10", "https://www.youtube.com/watch?v=maNziCIArqI&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=11", "https://www.youtube.com/watch?v=MSX29PxaYAg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=12", "https://www.youtube.com/watch?v=-kLHD0Qjih4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=13", "https://www.youtube.com/watch?v=tD5XEAXU_jg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=14", "https://www.youtube.com/watch?v=ERxSwJPgitA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=15", "https://www.youtube.com/watch?v=OAPMYZS1PyQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=16", "https://www.youtube.com/watch?v=T4cf_aLv0MA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=17", "https://www.youtube.com/watch?v=NyYf9rWSRSs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=18", "https://www.youtube.com/watch?v=qXLMylnUWY8&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=19", "https://www.youtube.com/watch?v=UjBqglMQDM4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=20", "https://www.youtube.com/watch?v=iM-Gvwpwufg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=21", "https://www.youtube.com/watch?v=NQ-5HE_KWwQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=22", "https://www.youtube.com/watch?v=xV_89Lz2qxs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=23", "https://www.youtube.com/watch?v=1vZ31315nms&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=24", "https://www.youtube.com/watch?v=tnclamheH4c&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=25", "https://www.youtube.com/watch?v=1PZ2GUWwd78&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=26", "https://www.youtube.com/watch?v=neyRa6Fmap4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=27", "https://www.youtube.com/watch?v=OEf9vGoKnVQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=28", "https://www.youtube.com/watch?v=4ZMmIqcHM54&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=29", "https://www.youtube.com/watch?v=8W6zrTQ-Fkw&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=30", "https://www.youtube.com/watch?v=hdu9tcCcrq0&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=31", "https://www.youtube.com/watch?v=Or-fInNS4XE&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=32"]
+LINKS = [
+    "https://www.youtube.com/watch?v=XEAE0FyNERA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=1",
+    "https://www.youtube.com/watch?v=pWyp6SAllY4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=2",
+    "https://www.youtube.com/watch?v=InN-bmRk2lQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=3",
+    "https://www.youtube.com/watch?v=bYpXWAsFYvM&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=4",
+    "https://www.youtube.com/watch?v=XucfklQa6iA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=5",
+    "https://www.youtube.com/watch?v=UC-lTdqXrh4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=6",
+    "https://www.youtube.com/watch?v=O7qWdIpcNTs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=7",
+    "https://www.youtube.com/watch?v=1ctdbP-ySWI&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=8",
+    "https://www.youtube.com/watch?v=4GQ2yo7KkEs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=9",
+    "https://www.youtube.com/watch?v=7Jq9b5nVhe4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=10",
+    "https://www.youtube.com/watch?v=maNziCIArqI&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=11",
+    "https://www.youtube.com/watch?v=MSX29PxaYAg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=12",
+    "https://www.youtube.com/watch?v=-kLHD0Qjih4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=13",
+    "https://www.youtube.com/watch?v=tD5XEAXU_jg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=14",
+    "https://www.youtube.com/watch?v=ERxSwJPgitA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=15",
+    "https://www.youtube.com/watch?v=OAPMYZS1PyQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=16",
+    "https://www.youtube.com/watch?v=T4cf_aLv0MA&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=17",
+    "https://www.youtube.com/watch?v=NyYf9rWSRSs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=18",
+    "https://www.youtube.com/watch?v=qXLMylnUWY8&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=19",
+    "https://www.youtube.com/watch?v=UjBqglMQDM4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=20",
+    "https://www.youtube.com/watch?v=iM-Gvwpwufg&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=21",
+    "https://www.youtube.com/watch?v=NQ-5HE_KWwQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=22",
+    "https://www.youtube.com/watch?v=xV_89Lz2qxs&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=23",
+    "https://www.youtube.com/watch?v=1vZ31315nms&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=24",
+    "https://www.youtube.com/watch?v=tnclamheH4c&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=25",
+    "https://www.youtube.com/watch?v=1PZ2GUWwd78&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=26",
+    "https://www.youtube.com/watch?v=OEf9vGoKnVQ&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=28",
+    "https://www.youtube.com/watch?v=neyRa6Fmap4&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=27",
+    "https://www.youtube.com/watch?v=4ZMmIqcHM54&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=29",
+    "https://www.youtube.com/watch?v=8W6zrTQ-Fkw&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=30",
+    "https://www.youtube.com/watch?v=hdu9tcCcrq0&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=31",
+    "https://www.youtube.com/watch?v=Or-fInNS4XE&list=PLmRDkQf8W1WFboD7M9xoLUf_pRwz6kL9c&index=32"
+]
 
 @app.route('/')
 def index():
